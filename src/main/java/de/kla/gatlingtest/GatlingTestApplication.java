f78756a2b87d5c02e264d1f09de5608391ef8c48
Kevin Lange <kevin.lange.1991@gmail.com>
2019-08-05 11:24:42 +0200

    initial commit

---
+++ src/main/java/de/kla/gatlingtest/GatlingTestApplication.java
@@ -0,0 +1,13 @@
+package de.kla.gatlingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatlingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatlingTestApplication.class, args);
+	}
+
+}
