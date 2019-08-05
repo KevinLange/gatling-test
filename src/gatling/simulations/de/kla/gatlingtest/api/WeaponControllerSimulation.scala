ba065ab797f024e211b2c948f349a4424b127e32
Kevin Lange <kevin.lange.1991@gmail.com>
2019-08-05 15:23:50 +0200

    moved simulation to the correct folder

--- src/gatling/simulations/de/kla/gatlingtest/api/WeaponControllerSimulation.scala
+++ src/gatling/simulations/de/kla/gatlingtest/api/WeaponControllerSimulation.scala
@@ -5,6 +5,7 @@ import io.gatling.http.Predef._
 
 class WeaponControllerSimulation extends Simulation {
   val httpConf = http.baseUrl("http://localhost:8080/api/")
+
   val scn = scenario("Get all weapons")
     .exec(http("request_1")
       .get("v1/weapon-management/weapons"))
